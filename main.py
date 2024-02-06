1a04d3852ba1bc7b7d0ae05794fc354a8acc0365
yashwanthreddy7178 <yashwanthreddy7178@gmail.com>
2024-02-05 22:10:50 -0500

    run success

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import mlflow
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+# Set the MLflow tracking URI and credentials again with the updated version
+#mlflow.set_tracking_uri('https://dagshub.com/yashwanthreddy7178/Wine-Quality-Prediction-MLOPS-with-MLflow.mlflow'))
 
+# Set environment variables for authentication
+#import os
+#os.environ['MLFLOW_TRACKING_USERNAME'] = 'yashwanthreddy7178'
+#os.environ['MLFLOW_TRACKING_PASSWORD'] = '2f0a09218b79018bb56442916a9544eb88a54c20'
 
 STAGE_NAME = "Data Ingestion stage"
 try:
--- src/mlProject/components/model_evaluation.py
+++ src/mlProject/components/model_evaluation.py
@@ -9,60 +9,67 @@ import joblib
 from mlProject.entity.config_entity import ModelEvaluationConfig
 from mlProject.utils.common import save_json
 from pathlib import Path
-
+#mlflow.set_tracking_uri('https://dagshub.com/yashwanthreddy7178/Wine-Quality-Prediction-MLOPS-with-MLflow.mlflow')
+os.environ['MLFLOW_TRACKING_USERNAME']
+os.environ['MLFLOW_TRACKING_PASSWORD']
 
 class ModelEvaluation:
     def __init__(self, config: ModelEvaluationConfig):
         self.config = config
 
-    
-    def eval_metrics(self,actual, pred):
+    def eval_metrics(self, actual, pred):
         rmse = np.sqrt(mean_squared_error(actual, pred))
         mae = mean_absolute_error(actual, pred)
         r2 = r2_score(actual, pred)
         return rmse, mae, r2
-    
-
 
     def log_into_mlflow(self):
+        try:
+            # Load test data and model
+            test_data = pd.read_csv(self.config.test_data_path)
+            model = joblib.load(self.config.model_path)
 
-        test_data = pd.read_csv(self.config.test_data_path)
-        model = joblib.load(self.config.model_path)
+            # Prepare test data
+            test_x = test_data.drop([self.config.target_column], axis=1)
+            test_y = test_data[[self.config.target_column]]
 
-        test_x = test_data.drop([self.config.target_column], axis=1)
-        test_y = test_data[[self.config.target_column]]
+            # Set MLflow registry URI
+            mlflow.set_registry_uri(self.config.mlflow_uri)
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+            #print(self.config.all_params)
+            mlflow.set_experiment('Wine-Quality-Prediction-Experiment1')
 
+            with mlflow.start_run():
+                # Make predictions
+                predicted_qualities = model.predict(test_x)
 
-        mlflow.set_registry_uri(self.config.mlflow_uri)
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+                # Evaluate metrics
+                (rmse, mae, r2) = self.eval_metrics(test_y, predicted_qualities)
 
+                # Save metrics as local JSON
+                scores = {"rmse": rmse, "mae": mae, "r2": r2}
+                save_json(path=Path(self.config.metric_file_name), data=scores)
+                try:
+                    mlflow.log_params(self.config.all_params)
+                    mlflow.log_metric("rmse", rmse)
+                    mlflow.log_metric("r2", r2)
+                    mlflow.log_metric("mae", mae)
 
-        with mlflow.start_run():
+                    if tracking_url_type_store != "file":
+                        mlflow.sklearn.log_model(model, "model", registered_model_name="ElasticnetModel")
+                    else:
+                        mlflow.sklearn.log_model(model, "model")
 
-            predicted_qualities = model.predict(test_x)
+                    print('Model evaluation and logging into MLflow completed successfully.')
 
-            (rmse, mae, r2) = self.eval_metrics(test_y, predicted_qualities)
-            
-            # Saving metrics as local
-            scores = {"rmse": rmse, "mae": mae, "r2": r2}
-            save_json(path=Path(self.config.metric_file_name), data=scores)
+                except mlflow.exceptions.MlflowException as e:
+                    print(f'MLflow exception: {e}')
+                except Exception as e:
+                    print(f'Error: {e}')
+                    raise e
 
-            mlflow.log_params(self.config.all_params)
-
-            mlflow.log_metric("rmse", rmse)
-            mlflow.log_metric("r2", r2)
-            mlflow.log_metric("mae", mae)
-
-
-            # Model registry does not work with file store
-            if tracking_url_type_store != "file":
-
-                # Register the model
-                # There are other ways to use the Model Registry, which depends on the use case,
-                # please refer to the doc for more information:
-                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                mlflow.sklearn.log_model(model, "model", registered_model_name="ElasticnetModel")
-            else:
-                mlflow.sklearn.log_model(model, "model")
+        except Exception as e:
+            print(f'Error: {e}')
+            raise e
 
     
